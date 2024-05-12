4a69e5b549320205133eb986e29d67adbeb52d25
Ramzey Burdette <ramzey.burdette2005@gmail.com>
2024-05-12 14:00:41 -0400

    added reading, writing and sockets to Server and Client

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,31 +1,36 @@
 package src.main.java;
 
-import java.net.*;
-import java.util.*;
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Client {
     private String alias;
     private final Inet4Address ipAddress;
+    private Socket clientSocket;
     private int port;
     private Server cliServer;
 
-    public Client() throws UnknownHostException {
+    public Client() throws UnknownHostException, UnsupportedOperationException {
         this.alias = "Anon";
         this.port = -1;
         this.ipAddress = (Inet4Address) Inet4Address.getLocalHost();
-        throw new UnsupportedOperationException("Not implemented!");
     }
 
-    public Client(String alias) throws UnknownHostException {
+    public Client(String alias) throws UnknownHostException, UnsupportedOperationException {
         this.alias = alias;
         this.port = -1;
         this.ipAddress = (Inet4Address) Inet4Address.getLocalHost();
-        throw new UnsupportedOperationException("Not implemented!");
     }
 
     public void changeAlias(String alias) {
         this.alias = alias;
-        this.cliServer.aliasDatabase.get(ipAddress).add(alias);
+        if (this.cliServer != null)
+            if (!this.cliServer.aliasDatabase.get(ipAddress).contains(alias))
+                this.cliServer.aliasDatabase.get(ipAddress).add(alias);
     }
 
     public String getAlias() {
@@ -43,20 +48,61 @@ public class Client {
     public boolean connect(int port) {
         this.port = port;
         try {
-            cliServer = Server.findServer(port);
-            if (!cliServer.aliasDatabase.get(ipAddress).contains(this.alias)) {
+
+            this.cliServer = Server.findServer(port);
+
+            if (!cliServer.aliasDatabase.get(ipAddress).contains(this.alias))
                 cliServer.aliasDatabase.get(ipAddress).add(this.alias);
-            }
-            return true;
+
+            this.clientSocket = this.cliServer.server().accept();
+
+            if (!this.clientSocket.isConnected())
+                this.clientSocket.connect(this.cliServer.getServerIPAddress());
+
+            return this.clientSocket.isConnected();
+
         } catch (Exception e) {
-            System.out.println(Arrays.toString(e.getStackTrace()));
+            System.out.println(e.getMessage());
+            return false;
+
+        }
+    }
+
+    public boolean disconnect() {
+        try {
+            this.clientSocket.close();
+            this.cliServer = null;
+            this.port = -1;
+            return this.clientSocket.isClosed();
+
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+            return this.clientSocket.isClosed();
+
+        }
+    }
+
+    public boolean send(String message) {
+        try (OutputStreamWriter writer = new OutputStreamWriter(this.clientSocket.getOutputStream());
+             BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+
+                return true;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
             return false;
         }
     }
 
-    public void disconnect() {
-        cliServer = null;
-        this.port = -1;
+    public String receive() {
+        try (InputStreamReader reader = new InputStreamReader(this.clientSocket.getInputStream());
+             BufferedReader bufferedReader = new BufferedReader(reader)) {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
-    
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,29 +1,51 @@
 package src.main.java;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.AutoCloseable;
 import java.net.*;
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-import com.google.gson.*;
+import com.google.gson.Gson;
 
-public class Server {
-    private final int port;
+public class Server implements AutoCloseable {
+    private static final int maxConnected = 2;
+    private static final ArrayList<Integer> prevServers = new ArrayList<>();
+
     public HashMap<Inet4Address, ArrayList<String>> aliasDatabase;
+    public HashMap<Inet4Address, String> messageDatabase;
+    private final int port;
+    private final ServerSocket serverSocket;
+    private final InetSocketAddress serverIPAddress;
+
 
     public Server(int port) {
         Gson gson = new Gson();
         this.port = port;
 
-        try (FileReader fileReader = new FileReader(String.format("src/main/resources/port%d.json", port))) {
+        try {
+            this.serverSocket = new ServerSocket(port, maxConnected);
+            this.serverIPAddress = new InetSocketAddress(InetAddress.getLocalHost(), port);
+            this.serverSocket.bind(this.serverIPAddress, maxConnected);
+            prevServers.add(port);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try (FileReader fileReader = new FileReader(String.format("src/main/resources/aliases/port%d.json", port))) {
             var json = gson.fromJson(fileReader, HashMap.class);
             this.aliasDatabase = new HashMap<>();
             for (Object key : json.keySet()) {
                 this.aliasDatabase.put(
                         ((Inet4Address) InetAddress.getByName((String) key)),
-                        (ArrayList<String>) json.get(key));
+                        (ArrayList<String>) json.get(key)
+                );
             }
         } catch (FileNotFoundException notFound) {
-            try (FileWriter fileWriter = new FileWriter(String.format("src/main/resources/port%d.json", port))) {
+            try (FileWriter fileWriter = new FileWriter(String.format("src/main/resources/aliases/port%d.json", port))) {
                 fileWriter.write("{}");
                 this.aliasDatabase = new HashMap<>();
             } catch (IOException ioe) {
@@ -33,20 +55,57 @@ public class Server {
             System.out.println(io.getMessage());
         }
 
+        try (FileReader fileReader = new FileReader(String.format("src/main/resources/messages/port%d.json", port))) {
+            var json = gson.fromJson(fileReader, HashMap.class);
+            for (Object key : json.keySet()) {
+                this.messageDatabase.put(
+                        ((Inet4Address) InetAddress.getByName((String) key)),
+                        (String) json.get(key)
+                );
+            }
+        } catch (FileNotFoundException notFound) {
+            try (FileWriter fileWriter = new FileWriter(String.format("src/main/resources/message/port%d.json", port))) {
+                fileWriter.write("{}");
+                this.aliasDatabase = new HashMap<>();
+            } catch (IOException ioe) {
+                System.out.println(ioe.getMessage());
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+        }
+
         throw new UnsupportedOperationException("Not implemented!");
     }
 
     public static Server findServer(int port) {
-        throw new UnsupportedOperationException("Not implemented!");
+        for (Integer prevServer : prevServers)
+            if (port == prevServer)
+                return new Server(prevServer);
+
+        System.out.println("Server didn't exist; creating new server"); // include ...
+
+        return new Server(port);
     }
 
-    public boolean close() throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Not implemented!");
+    public boolean close() {
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
     }
 
     public int getPort() {
         return this.port;
     }
 
-}
+    public ServerSocket server() {
+        return this.serverSocket;
+    }
 
+    public InetSocketAddress getServerIPAddress() {
+        return this.serverIPAddress;
+    }
+
+
+}
